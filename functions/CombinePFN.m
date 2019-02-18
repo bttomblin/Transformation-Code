7232f36f8d6a975c81478d99ecf854d0888e2bde
Tanner Filben <tfilben@wakehealth.edu>
2019-02-18 17:10:05 -0500

    updating github with changes made to main directory
    Brian made two bug fixes.

--- functions/CombinePFN.m
+++ functions/CombinePFN.m
@@ -5,12 +5,13 @@ masterLoc = fileparts(DataFolders{1,1});
 PFN_all = {};
 for i = 1:length(DataFolders)
     load(fullfile(DataFolders{1,i},'01_PFN.mat'))
-      
-    if(isempty(PFN_all))
-        PFN_all = PFN;
-    else
-        PFN_all = outerjoin(PFN_all,PFN,'MergeKeys',true);
-    end    
+    if isempty(PFN) == 0  
+        if(isempty(PFN_all))
+            PFN_all = PFN;
+        else
+            PFN_all = outerjoin(PFN_all,PFN,'MergeKeys',true);
+        end    
+    end
 end
 
 save(fullfile(masterLoc,'01_PFN.mat'), 'PFN_all');
