651d23d14db01146677a740e8fa3b59482fe42c2
Tanner Filben <tfilben@wakehealth.edu>
2019-04-10 16:08:56 -0400

    updating with files from main directory

--- functions/ExportImpactsToTable.m
+++ functions/ExportImpactsToTable.m
@@ -9,43 +9,37 @@ end
 
 % potential columns: session type, player ID (diff than MP?), peak to peak,
 % team, age, position, film time
-impactVarNames = {'MouthpieceID','Description','Classification','Date','Time','ImpactNumber','LinAccX','LinAccY','LinAccZ','LinAccRes','RotVelX','RotVelY','RotVelZ','RotVelRes','RotAccX','RotAccY','RotAccZ','RotAccRes','MetThreshold'};
+impactVarNames = {'MouthpieceID','Date','Time','ImpactNumber','LinAccX','LinAccY','LinAccZ','LinAccRes','RotVelX','RotVelY','RotVelZ','RotVelRes','RotAccX','RotAccY','RotAccZ','RotAccRes'};
 
-
-load(fullfile(folder,'00_transformedData.mat'))
+load(fullfile(folder,'00_processedData.mat'))
 
 impactTable = {};
 for j = 1:length(impacts)
-
-    if(isfield(impacts{1,j},'PeakValues') && isfield(impacts{1,j},'FilmReview')) % only export events that have been transformed and reviewed
-
-        impactTable(j,:) = {impacts{1,j}.Info.MouthpieceID, impacts{1,j}.FilmReview.ImpactType, impacts{1,j}.FilmReview.ImpactClass, impacts{1,j}.Info.ImpactDate, impacts{1,j}.Info.ImpactTime, ...
+        impactTable(j,:) = {impacts{1,j}.Info.MouthpieceID, impacts{1,j}.Info.ImpactDate, impacts{1,j}.Info.ImpactTime, ...
             impacts{1,j}.Info.ImpactIndex+1, impacts{1,j}.PeakValues.LinAccX, impacts{1,j}.PeakValues.LinAccY, impacts{1,j}.PeakValues.LinAccZ, impacts{1,j}.PeakValues.LinAcc, ... 
             impacts{1,j}.PeakValues.RotVelX,impacts{1,j}.PeakValues.RotVelY,impacts{1,j}.PeakValues.RotVelZ,impacts{1,j}.PeakValues.RotVel,impacts{1,j}.PeakValues.RotAccX, ... 
-            impacts{1,j}.PeakValues.RotAccY,impacts{1,j}.PeakValues.RotAccZ,impacts{1,j}.PeakValues.RotAcc,impacts{1,j}.Info.MetThreshold};
-
-    end
+            impacts{1,j}.PeakValues.RotAccY,impacts{1,j}.PeakValues.RotAccZ,impacts{1,j}.PeakValues.RotAcc};
 end
 
 if(~isempty(impactTable))
-    impactTable = cell2table(impactTable,'VariableNames',impactVarNames);
-    impactTable = impactTable(~cellfun(@isempty,impactTable.MouthpieceID),:); % gets rid of empty rows from untransformed impacts
-    impactTable.Date = datetime(impactTable.Date,'InputFormat','yyyyMMdd','Format','MM/dd/yyyy');
+    dates = impactTable(:,2);
+    for i = 1:length(dates)
+        temp = cellstr(datetime(dates{i},'InputFormat','yyyyMMdd','Format','MM/dd/yyyy'));
+        newDates{i,1} = temp{1};
+    end
+    impactTable(:,2) = newDates;
 
-    if(exist(fullfile(folder,strcat('impacts_',impacts{1,1}.Info.ImpactDate,'.txt'))))
-%         msg = cell(4,1);
-%         msg{1,1} = sprintf('Impact export already exists for this date.');
-%         msg{2,1} = sprintf('%s',impacts{1,1}.Info.ImpactDate);
-%         msgbox(msg);
-    elseif(exist(fullfile(folder,strcat('impacts_',impacts{1,1}.Info.ImpactDate,'-',impacts{1,end}.Info.ImpactDate,'.txt'))))
-%         msg = cell(4,1);
-%         msg{1,1} = sprintf('Impact export already exists for this date range.');
-%         msg{2,1} = sprintf('%s',strcat(impacts{1,1}.Info.ImpactDate,'-',impacts{1,end}.Info.ImpactDate));
-%         msgbox(msg);
+    times = impactTable(:,3);
+    newTimes=strcat('''',times);
+    impactTable(:,3) = newTimes;
+    OUTPUT = vertcat(impactVarNames,impactTable);
+    
+    if(exist(fullfile(folder,strcat('impacts_',impacts{1,1}.Info.ImpactDate,'.xls'))))
+    elseif(exist(fullfile(folder,strcat('impacts_',impacts{1,1}.Info.ImpactDate,'-',impacts{1,end}.Info.ImpactDate,'.xls'))))
     elseif(strcmp(impacts{1,1}.Info.ImpactDate, impacts{1,end}.Info.ImpactDate) == 1)
-        writetable(impactTable,fullfile(folder,strcat('impacts_',impacts{1,1}.Info.ImpactDate,'.txt')))
+        xlswrite(fullfile(folder,strcat('impacts_',impacts{1,1}.Info.ImpactDate,'.xls')),OUTPUT)
     else
-        writetable(impactTable,fullfile(folder,strcat('impacts_',impacts{1,1}.Info.ImpactDate,'-',impacts{1,end}.Info.ImpactDate,'.txt')))
+        xlswrite(fullfile(folder,strcat('impacts_',impacts{1,1}.Info.ImpactDate,'-',impacts{1,end}.Info.ImpactDate,'.xls')),OUTPUT)
     end   
     
 end
