2ad32e6e2047b50c43656a8076db0476dfdf0ad0
btomblin <btomblin@wakehealth.edu>
2019-04-19 10:35:01 -0400

    Final Changes to Transform to Work with Hockey
    Adds the Head Contact variable into the outputs. Also added ability to continue if individual MPs not film reviewed

--- functions/ExportPFN.m
+++ functions/ExportPFN.m
@@ -23,17 +23,32 @@ for i=1:length(mps)
     MouthpieceID = string(mps{i});
     currMP = dateFR.(mps{i});
     if(any(strcmp(currMP.Impact_Class,"PFN")))
-        temp = currMP(strcmp(currMP.Impact_Class,"PFN"),2:4);
-        temp = addvars(temp,repmat(MouthpieceID,height(temp),1),'Before','Impact_Type');
-        temp = addvars(temp,repmat(date,height(temp),1),'Before','Film_Time');
-        
-        temp.Properties.VariableNames{'Var1'} = 'MouthpieceID';
-        temp.Properties.VariableNames{'Var3'} = 'Date';
-        temp.Properties.VariableNames{'Impact_Type'} = 'Description';
-        temp.Properties.VariableNames{'Film_Time'} = 'FilmTime';
-        temp.Properties.VariableNames{'Impact_Time'} = 'Time';
-        %HERE^
-        temp = temp(:,[1:3,5,4]);
+        if isequal(sport,"hockey")
+            temp = currMP(strcmp(currMP.Impact_Class,"PFN"),[2:4,6]);
+            temp = addvars(temp,repmat(MouthpieceID,height(temp),1),'Before','Impact_Type');
+            temp = addvars(temp,repmat(date,height(temp),1),'Before','Film_Time');
+
+            temp.Properties.VariableNames{'Var1'} = 'MouthpieceID';
+            temp.Properties.VariableNames{'Var3'} = 'Date';
+            temp.Properties.VariableNames{'Impact_Type'} = 'Description';
+            temp.Properties.VariableNames{'Film_Time'} = 'FilmTime';
+            temp.Properties.VariableNames{'Impact_Time'} = 'Time';
+            temp.Properties.VariableNames{'Head_Contact'} = 'HeadContact';
+            
+            temp = temp(:,[1,2,6,3,5,4]);
+        else
+            temp = currMP(strcmp(currMP.Impact_Class,"PFN"),2:4);
+            temp = addvars(temp,repmat(MouthpieceID,height(temp),1),'Before','Impact_Type');
+            temp = addvars(temp,repmat(date,height(temp),1),'Before','Film_Time');
+
+            temp.Properties.VariableNames{'Var1'} = 'MouthpieceID';
+            temp.Properties.VariableNames{'Var3'} = 'Date';
+            temp.Properties.VariableNames{'Impact_Type'} = 'Description';
+            temp.Properties.VariableNames{'Film_Time'} = 'FilmTime';
+            temp.Properties.VariableNames{'Impact_Time'} = 'Time';
+            %HERE^
+            temp = temp(:,[1:3,5,4]);
+        end
         
         if(isempty(PFN))
             PFN = temp;
