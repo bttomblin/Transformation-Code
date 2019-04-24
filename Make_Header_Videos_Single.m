76ff12bc6634bed8f09ae242c2dccb3a1b89b38e
btomblin <btomblin@wakehealth.edu>
2019-04-24 13:32:50 -0400

    Added Single Video script
    Currently is still manual for video detail inputs. Also only for soccer

--- Make_Header_Videos_Single.m
+++ Make_Header_Videos_Single.m
@@ -2,97 +2,95 @@ close all
 clearvars -except impacts
 clc
 
-PreImpactTime = 3;
+PreImpactTime = 1;
 PostImpactTime = 3;
 
-% Name of impact as string as found in Reviews
-impactname = 'Header';
+% Date of impact as string as found in Info
+impactdate = '20181104';
 
-% Name if impact for Video Files (no spaces)
-impactvideo = 'Header';
+% MP of impact as string as found in Info
+impactmp = 'MP083';
+
+% Time of impact as string as found in Info
+impacttime = '08:49:43.000';
+
+% Impact Index (Impact Number - 1)
+impactindex = 4;
 
 % Path to transformed data
 Path = '\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\Merged_Analysis\Fall2018_Merged_transformedData.mat';
 
 %% Load and make new impact list
-
 if exist('impacts','var') == 0
 load(Path);
 end
 
-ii = 1;
+main = [];
 for i = 1:length(impacts)
-   if isequal(impacts{1,i}.FilmReview.ImpactType,impactname)
-       Impact_List{ii,1} = impacts{1,i};
-       OI_List{ii,1} = i;
-       ii = ii+1;
+   if isequal(impacts{1,i}.Info.ImpactDate,impactdate)
+       if isequal(impacts{1,i}.Info.MouthpieceID,impactmp)
+           if isequal(impacts{1,i}.Info.ImpactTime,impacttime)
+               if impacts{1,i}.Info.ImpactIndex == impactindex
+                   main = impacts{1,i};
+                   break
+               end
+           end
+       end
    end
 end
 
-%% Make New Directory
-folderpath = strcat(cd,'\','Video_Outputs','\',impactvideo);
-    mkdir(folderpath)
+if isempty(main)
+    error('No Impact Found')
+end
 
 %% Make Table of Impacts
-
-for count1 = 1:length(Impact_List)
-    % counter
-        if count1 < 10
-            Label_List{count1,1} = sprintf('%s_00%d',impactvideo,count1);
-        elseif count1 < 100
-            Label_List{count1,1} = sprintf('%s_0%d',impactvideo,count1);
-        else
-            Label_List{count1,1} = sprintf('%s_%d',impactvideo,count1);
-        end
+    % Label
+        Label = main.FilmReview.ImpactType;
     % MP
-        MP_List{count1,1} = Impact_List{count1,1}.FilmReview.MouthpieceID;
+        MP = main.FilmReview.MouthpieceID;
     % Date
-        Date = Impact_List{count1,1}.FilmReview.ImpactDate;
+        Date = main.FilmReview.ImpactDate;
             if ~contains(Date,'Game')
                 t = datetime(Date,'InputFormat','MMM_dd_yy');
                 s = datetime(t,'Format','yyyy-MM-dd');
-                Date_List{count1,1} = char(s);
-                Game_List{count1,1} = 'No Game Number';
+                Date = char(s);
+                Game = 'No Game Number';
             else
                 g = strfind(Date,'Game');
-                    Game_List{count1,1} = Date(g:g+4);
+                    Game = Date(g:g+4);
                     Date = strcat(Date(1:g-1),Date(length(Date)-1:length(Date)));
                 t = datetime(Date,'InputFormat','MMM_dd_yy');
                 s = datetime(t,'Format','yyyy-MM-dd');
-                Date_List{count1,1} = char(s);
+                Date = char(s);
             end
     % Impact Number
-        I_List{count1,1} = Impact_List{count1,1}.FilmReview.ImpactNumber;        
+        I = main.FilmReview.ImpactNumber;        
     % Actual Time
-        ATList{count1,1} = char(Impact_List{count1,1}.FilmReview.VideoImpactTime);
+        AT = char(main.FilmReview.VideoImpactTime);
     % Video Time
-        VTList{count1,1} = char(Impact_List{count1,1}.FilmReview.FilmTime);
-end
+        VT = char(main.FilmReview.FilmTime);
 
-Table = table(Label_List,MP_List,Date_List,Game_List,I_List,VTList,ATList,OI_List,'VariableNames',...
-    {'Video_Label' 'MP' 'Date' 'Game_Number' 'Impact_Number' 'Video_Time' 'Actual_Time' 'Total_Impact_Number'});
-
-xlsxname = strcat(folderpath,'\',impactvideo,'_Index_List.xlsx');
-writetable(Table,xlsxname);
+        temp = {Label,MP,Date,Game,I,VT,AT,i};
+        
+Table = cell2table(temp,'VariableNames',{'Label' 'MP' 'Date' 'Game_Number' 'Impact_Number' 'Video_Time' 'Actual_Time' 'Total_Impact_Number'});
 
 %% Make new videos
 
-for count2 = 1:height(Table)
     % Find Video Folder based on MP Team
-        if contains(Table.MP(count2,1),'092') || contains(Table.MP(count2,1),'093') || contains(Table.MP(count2,1),'103') || contains(Table.MP(count2,1),'110')
-            if contains(Table.Game_Number(count2,1),'No')
-                VideoFolder = strcat('\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\2018_Fall_U14\Film_Review\',char(Table.Date(count2,1)));
+        if contains(Table.MP,'092') || contains(Table.MP,'093') || contains(Table.MP,'103') || contains(Table.MP,'110')
+            if contains(Table.Game_Number,'No')
+                VideoFolder = strcat('\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\2018_Fall_U14\Film_Review\',char(Table.Date));
                     addpath(VideoFolder)
             else
-                VideoFolder = strcat('\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\2018_Fall_U14\Film_Review\',char(Table.Date(count2,1)),'_',char(Table.Game_Number(count2,1)));
+                VideoFolder = strcat('\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\2018_Fall_U14\Film_Review\',char(Table.Date),'_',char(Table.Game_Number));
                     addpath(VideoFolder)
             end
         else
-            if contains(Table.Game_Number(count2,1),'No')
-                VideoFolder = strcat('\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\2018_Fall_U16\Film_Review\',char(Table.Date(count2,1)));
+            if contains(Table.Game_Number,'No')
+                VideoFolder = strcat('\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\2018_Fall_U16\Film_Review\',char(Table.Date));
                     addpath(VideoFolder)
             else
-                VideoFolder = strcat('\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\2018_Fall_U16\Film_Review\',char(Table.Date(count2,1)),'_',char(Table.Game_Number(count2,1)));
+                VideoFolder = strcat('\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\2018_Fall_U16\Film_Review\',char(Table.Date),'_',char(Table.Game_Number));
                     addpath(VideoFolder)
             end
         end
@@ -107,7 +105,7 @@ for count2 = 1:height(Table)
         end
         
     % Video Start/Stop times of new videos (+-1 sec from impact time)
-        VideoTime = char(Table.Video_Time(count2,1));
+        VideoTime = char(Table.Video_Time);
             VT_1 = duration(VideoTime,'InputFormat','hh:mm:ss');
             VT_2 = seconds(VT_1);
             StartTime = VT_2 - PreImpactTime;
@@ -117,10 +115,11 @@ for count2 = 1:height(Table)
         % If only 1 video on that day
         if length(File_Structure) == 1
             clc;
-            fprintf('Making Video for %s: %d/%d\n',impactname,count2,height(Table))
             
+            fprintf('Making Video \n')
+
             inputName = strcat(File_Structure.folder,'\',File_Structure.name);
-            outputName = strcat(folderpath,'\',char(Table.Video_Label(count2,1)),'.mp4');
+            outputName = strcat(cd,'\','SingleImpact','.mp4');
             
             a = VideoReader(inputName);
             beginFrame = StartTime * a.FrameRate;
@@ -141,7 +140,7 @@ for count2 = 1:height(Table)
         % event based on it's impact time
         else
             clc;
-                if contains(Table.MP(count2,1),'092') || contains(Table.MP(count2,1),'093') || contains(Table.MP(count2,1),'103') || contains(Table.MP(count2,1),'110')
+                if contains(Table.MP,'092') || contains(Table.MP,'093') || contains(Table.MP,'103') || contains(Table.MP,'110')
                     SessionTimes = readtable('\\medctr\DFS\cib$\shared\02_projects\mouthpiece_data_collection\soccer\2018_Soccer_Fall\Merged_Analysis\U14_Session_Times.xlsx');
                         SessionTimes.Video_2_Start = datestr(SessionTimes.Video_2_Start,'HH:MM:SS');
                         SessionTimes.Date = datestr(SessionTimes.Date,'yyyy-mm-dd');
@@ -151,12 +150,12 @@ for count2 = 1:height(Table)
                         SessionTimes.Date = datestr(SessionTimes.Date,'yyyy-mm-dd');
                 end
             
-            fprintf('Making Video for  %s: %d/%d\n',impactname,count2,height(Table))
+            fprintf('Making Video \n')
             
             for dt = 1:height(SessionTimes)
-                if contains(Table.Game_Number(count2,:),'No')
-                    if contains(SessionTimes.Date(dt,:),char(Table.Date(count2,:)))
-                        if datetime(SessionTimes.Video_2_Start(dt,:)) > datetime(char(Table.Actual_Time(count2,1)))
+                if contains(Table.Game_Number,'No')
+                    if contains(SessionTimes.Date(dt,:),char(Table.Date))
+                        if datetime(SessionTimes.Video_2_Start(dt,:)) > datetime(char(Table.Actual_Time))
                             inputName = strcat(File_Structure(1).folder,'\',File_Structure(1).name);
                             break
                         else
@@ -164,9 +163,9 @@ for count2 = 1:height(Table)
                             break
                         end
                     end
-                elseif contains(Table.Game_Number(count2,:),'1')
-                    if contains(SessionTimes.Date(dt,:),char(Table.Date(count2,:))) && contains(SessionTimes.Game_Number(dt,:),'1')
-                        if datetime(SessionTimes.Video_2_Start(dt,:)) > datetime(char(Table.Actual_Time(count2,1)))
+                elseif contains(Table.Game_Number,'1')
+                    if contains(SessionTimes.Date(dt,:),char(Table.Date)) && contains(SessionTimes.Game_Number(dt,:),'1')
+                        if datetime(SessionTimes.Video_2_Start(dt,:)) > datetime(char(Table.Actual_Time))
                             inputName = strcat(File_Structure(1).folder,'\',File_Structure(1).name);
                             break
                         else
@@ -174,9 +173,9 @@ for count2 = 1:height(Table)
                             break
                         end
                     end
-                elseif contains(Table.Game_Number(count2,:),'2')
-                    if contains(SessionTimes.Date(dt,:),char(Table.Date(count2,:))) && contains(SessionTimes.Game_Number(dt,:),'2')
-                        if datetime(SessionTimes.Video_2_Start(dt,:)) > datetime(char(Table.Actual_Time(count2,1)))
+                elseif contains(Table.Game_Number,'2')
+                    if contains(SessionTimes.Date(dt,:),char(Table.Date)) && contains(SessionTimes.Game_Number(dt,:),'2')
+                        if datetime(SessionTimes.Video_2_Start(dt,:)) > datetime(char(Table.Actual_Time))
                             inputName = strcat(File_Structure(1).folder,'\',File_Structure(1).name);
                             break
                         else
@@ -189,7 +188,7 @@ for count2 = 1:height(Table)
                 end
             end
             
-            outputName = strcat(folderpath,'\',char(Table.Video_Label(count2,1)),'.mp4');
+            outputName = strcat(cd,'\','SingleImpact','.mp4');
             
             a = VideoReader(inputName);
             beginFrame = StartTime * a.FrameRate;
@@ -207,11 +206,10 @@ for count2 = 1:height(Table)
             
             close(vidObj);
         end
-end
 
 %% End
 clc
 
-fprintf('Finished: %d %s videos made\n',height(Table),impactname)
+fprintf('Finished \n')
 
-clearvars -except Table folderpath impactname
+clearvars -except Table impacts
